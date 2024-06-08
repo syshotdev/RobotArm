e4fc7b3370c30b341b6eb6e3e26327beaaf6cb89
syshotdev <syshotdev@gmail.com>
2024-06-08 09:34:57 -0700

    Everything changed so committing before big commit

--- Scripts/RobotArmSegment.gd
+++ Scripts/RobotArmSegment.gd
@@ -5,19 +5,44 @@ class_name RobotArmSegment
 @export var servo : Constants.ServoIndex
 @export var minPosition : int = 0
 @export var maxPosition : int = 1000
+@export var minDegrees : float = 0
+@export var maxDegrees : float = 180
+
 @export var lengthCM : int = 20 # Centimeters, in Godot meters divide by 10
+@export var nextArmSegment : RobotArmSegment = null
 #@export var rotationAxis : Vector3i
 
-var servoPosition : int = floor(maxPosition / 2.0) : set = Move
+var servoPosition : int = minPosition : set = Move
 var segment : CSGBox3D
 
 
 func _ready():
   segment = $Segment
   segment.size.x = lengthCM / 10.0
+  # Make the base at start of last arm
+  segment.position.x = -(lengthCM / (10.0 * 2))
+
+  Move(self.servoPosition) # To update rotation
+
+  if nextArmSegment == null:
+    return
+
+  nextArmSegment.position.x = -self.lengthCM / 10.0
 
 
 func Move(newServoPosition : int):
   servoPosition = clampi(newServoPosition, minPosition, maxPosition)
-  rotation.z = servoPosition
 
+  var rotationDegreesFromPositionAndDegrees = MapRange(servoPosition, minPosition, maxPosition, minDegrees, maxDegrees)
+
+  rotation.z = deg_to_rad(rotationDegreesFromPositionAndDegrees)
+
+
+# A range from f1(first1) to f2 is now mapped to s1(second1) to s2,
+# Example: (50, 0, 100, 100, 1000). Mapping (0 to 100) to (100 to 1000), and return what 50 would be. 
+# This will return half of 1000 minus 100, being 450.
+func MapRange(input, f1, f2, s1, s2) -> float:
+  # https://stackoverflow.com/questions/5731863/mapping-a-numeric-range-onto-another
+  var slope : float = (s1 - s2) / (f1 - f2)
+  var output : float = s1 + slope * (input - f1)
+  return output
--- Scripts/ServoCommunicator.cs
+++ Scripts/ServoCommunicator.cs
@@ -4,73 +4,73 @@ using Godot;
 
 public partial class ServoCommunicator : Node
 {
-    const int BaudRate = 2400;
-    const int DataBits = 8; // I don't remember the amount.  EDIT: I still don't know it
-    const string portName = "/dev/ttyUSB0";
+	const int BaudRate = 2400;
+	const int DataBits = 8; // I don't remember the amount.  EDIT: I still don't know it
+	const string portName = "/dev/ttyUSB0";
 
-    private SerialPort port;
+	private SerialPort port;
 
-    public override void _Ready()
-    {
-        port = new SerialPort();
+	public override void _Ready()
+	{
+		port = new SerialPort();
 
-        port.BaudRate = BaudRate;
-        port.DataBits = DataBits;
-        port.StopBits = StopBits.Two;
-        port.Parity = Parity.None;
+		port.BaudRate = BaudRate;
+		port.DataBits = DataBits;
+		port.StopBits = StopBits.Two;
+		port.Parity = Parity.None;
 
-        // This stuff was from official docs, which is why it is confusing
-        port.Handshake = Handshake.None;
-        port.RtsEnable = true;
+		// This stuff was from official docs, which is why it is confusing
+		port.Handshake = Handshake.None;
+		port.RtsEnable = true;
 
-        port.DataReceived += new SerialDataReceivedEventHandler(ReceivedMessage);
+		port.DataReceived += new SerialDataReceivedEventHandler(ReceivedMessage);
 
-        // This is "temporary" but I won't fix it.
-        ChangePort(portName);
-    }
+		// This is "temporary" but I won't fix it.
+		ChangePort(portName);
+	}
 
-    private void TestFunction()
-    {
-        GD.Print(SerialPort.GetPortNames());
-        ChangePort("/dev/ttyUSB0");
-        SendMessage("!SCVER?" + Convert.ToChar(13));
-    }
+	private void TestFunction()
+	{
+		GD.Print(SerialPort.GetPortNames());
+		ChangePort("/dev/ttyUSB0");
+		SendMessage("!SCVER?" + Convert.ToChar(13));
+	}
 
-    public void ChangePort(string portName)
-    {
-        port.Close();
-        port.PortName = portName;
-        port.Open();
-    }
+	public void ChangePort(string portName)
+	{
+		port.Close();
+		port.PortName = portName;
+		port.Open();
+	}
 
-    public void SendCommand(int servoIndex, int position, int ramp)
-    {
-        byte positionLowBit = Convert.ToByte(position % 255);
-        byte positionHighBit = Convert.ToByte(position >> 8);
-        byte[] byteMessage = new byte[4];
-        // I really wish I could put this in brackets :( []
-        // First ServoIndex 1 byte, Ramp 1 byte, Position is 2 bytes
-        byteMessage[0] = Convert.ToByte(servoIndex);
-        byteMessage[1] = Convert.ToByte(ramp);
-        byteMessage[2] = positionLowBit;
-        byteMessage[3] = positionHighBit;
+	public void SendCommand(int servoIndex, int position, int ramp)
+	{
+		byte positionLowBit = Convert.ToByte(position % 255);
+		byte positionHighBit = Convert.ToByte(position >> 8);
+		byte[] byteMessage = new byte[4];
+		// I really wish I could put this in brackets :( []
+		// First ServoIndex 1 byte, Ramp 1 byte, Position is 2 bytes
+		byteMessage[0] = Convert.ToByte(servoIndex);
+		byteMessage[1] = Convert.ToByte(ramp);
+		byteMessage[2] = positionLowBit;
+		byteMessage[3] = positionHighBit;
 
-        // Example message: !SC(0)(63)(1)(255)(13) to make servo 0 to pos 511
-        string message = "!SC" + byteMessage.GetStringFromUtf8() + Convert.ToChar(13);
-        port.WriteLine(message);
-    }
+		// Example message: !SC(0)(63)(1)(255)(13) to make servo 0 to pos 511
+		string message = "!SC" + byteMessage.GetStringFromUtf8() + Convert.ToChar(13);
+		port.WriteLine(message);
+	}
 
-    public void SendMessage(string message)
-    {
-        port.WriteLine(message);
-    }
+	public void SendMessage(string message)
+	{
+		port.WriteLine(message);
+	}
 
-    // Technically this can be static but I want to use THIS class's port.
-    private void ReceivedMessage(object sender, SerialDataReceivedEventArgs e)
-    {
-        SerialPort tempPort = (SerialPort)sender;
-        string indata = tempPort.ReadExisting();
-        GD.Print("Data Recieved:");
-        GD.Print(indata);
-    }
+	// Technically this can be static but I want to use THIS class's port.
+	private void ReceivedMessage(object sender, SerialDataReceivedEventArgs e)
+	{
+		SerialPort tempPort = (SerialPort)sender;
+		string indata = tempPort.ReadExisting();
+		GD.Print("Data Recieved:");
+		GD.Print(indata);
+	}
 }
